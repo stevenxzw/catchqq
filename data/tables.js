f579f6ffaba4d9e1f819904868737063a21a48d9
stevenxzw <stevenxzw@gmail.com>
2014-06-30 21:37:58 +0800

    catch get blog qq

--- data/tables.js
+++ data/tables.js
@@ -14,7 +14,7 @@
             loginTimes : 1
         }],
 
-        blogqq : [,{
+        blogqq : [{
             qq : '79186391',
             blogid : '6c06bc1ac125aa5305030e00',
             name : 'test',
