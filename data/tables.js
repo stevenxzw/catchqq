2d49b1dafa931d45097f4591c31e33e260602971
xzw <stevenxzw@sina.com.cn>
2014-07-10 00:16:35 +0800

    数据结构

--- data/tables.js
+++ data/tables.js
@@ -15,13 +15,104 @@
         }],
 
         blogqq : [{
-            qq : '154036777',
-            blogid : '6c06bc1ac125aa5305030e00',
-            name : 'test',
+            qq : '154036777',//访客QQ
+            blogid : '6c06bc1ac125aa5305030e00',//blog id
+            name : 'test',//访客名称
+            qzoneid : '448530028',//blog主空间ID
+            addTime : 1404921913274,//添加时间
             blogname : '测试',
-            area : '',
-            time : '1403854824273'
-        }]
+            area : '',//地区
+            time : '1403854824273'//访客访问时间
+        },    {
+            "qq": 374224897,
+            "name": "Up",
+            qzoneid : '448530028',//blog主空间ID
+            addTime : 1404921913275,//添加时间
+            "blogid": "6c06bc1ae975b353c45c0d00",
+            "blogname": "酷酷口语",
+            "area": "",
+            "time": 1404900709
+        },
+            {
+                "qq": 853375132,
+                "name": "雪之梦幻",
+                qzoneid : '448530028',//blog主空间ID
+                addTime : 1404921913274,//添加时间
+                "blogid": "6c06bc1ae975b353c45c0d00",
+                "blogname": "酷酷口语",
+                "area": "",
+                "time": 1404897835
+            },
+            {
+                "qq": 466561183,
+                "name": "不浪漫罪名",
+                qzoneid : '448530028',//blog主空间ID
+                addTime : 1404921913274,//添加时间
+                "blogid": "6c06bc1ae975b353c45c0d00",
+                "blogname": "酷酷口语",
+                "area": "",
+                "time": 1404896057
+            },
+            {
+                "qq": 529958673,
+                "name": "暖阳",
+                qzoneid : '448530028',//blog主空间ID
+                addTime : 1404921913274,//添加时间
+                "blogid": "6c06bc1ae975b353c45c0d00",
+                "blogname": "酷酷口语",
+                "area": "",
+                "time": 1404893389
+            },
+            {
+                "qq": 15058194,
+                "name": "平安福",
+                qzoneid : '448530028',//blog主空间ID
+                addTime : 1404921913274,//添加时间
+                "blogid": "6c06bc1ae975b353c45c0d00",
+                "blogname": "酷酷口语",
+                "area": "",
+                "time": 1404884502
+            },
+            {
+                "qq": 386918847,
+                "name": "~筱筱~",
+                qzoneid : '448530028',//blog主空间ID
+                addTime : 1404921913274,//添加时间
+                "blogid": "6c06bc1ae975b353c45c0d00",
+                "blogname": "酷酷口语",
+                "area": "",
+                "time": 1404877548
+            },
+            {
+                "qq": 237328499,
+                "name": "统一面馆",
+                qzoneid : '448530028',//blog主空间ID
+                addTime : 1404921913274,//添加时间
+                "blogid": "6c06bc1ae975b353c45c0d00",
+                "blogname": "酷酷口语",
+                "area": "",
+                "time": 1404356440
+            },
+            {
+                "qq": 465912122,
+                "name": "堂茗香",
+                qzoneid : '448530028',//blog主空间ID
+                addTime : 1404921913274,//添加时间
+                "blogid": "6c06bc1ae975b353c45c0d00",
+                "blogname": "酷酷口语",
+                "area": "",
+                "time": 1404356275
+            },
+            {
+                "qq": 136641563,
+                "name": "   Mark",
+                qzoneid : '448530028',//blog主空间ID
+                addTime : 1404921913274,//添加时间
+                "blogid": "6c06bc1ae975b353c45c0d00",
+                "blogname": "酷酷口语",
+                "area": "",
+                "time": 1404356016
+            }]
     };
 
 })()
